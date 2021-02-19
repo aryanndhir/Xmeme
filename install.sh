ad480891d2da87f45bd75f24c80b3123fd716093
Aryann Dhir <59336767+aryanndhir@users.noreply.github.com>
2021-02-19 11:48:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 # Any installation related commands
 
-sudo apt-get update
-sudo apt-get install -y python3
-sudo apt install -y python3-pip
-pip3 install -r requirements.txt
+sudo apt update
 
+sudo apt install -y python3-pip python3 python3-dev libpq-dev nginx curl
+
+sudo -H pip3 install --upgrade pip
+
+sudo -H pip3 install virtualenv
